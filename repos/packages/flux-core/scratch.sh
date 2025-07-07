441f5de6ee981a00f0603a1c0ec29c90e6a22c84
lucaparisi91 <luca.parisi91@gmail.com>
2025-07-07 17:52:38 +0100

    Added fix to flux-core in order to build with spack

---
+++ repos/packages/flux-core/scratch.sh
@@ -0,0 +1,5 @@
+export LIBRARY_PATH=/work/z19/z19/lparisi/.spack-0.23.0/opt/spack/linux-sles15-zen2/gcc-11.2.0/libiconv-1.17-suuqhan57tcpbrs4px6rzksm75kr4n5r/lib:$LIBRARY_PATH
+export CPATH=/work/z19/z19/lparisi/.spack-0.23.0/opt/spack/linux-sles15-zen2/gcc-11.2.0/libiconv-1.17-suuqhan57tcpbrs4px6rzksm75kr4n5r/include:$CPATH
+
+LIBARCHIVE_CFLAGS=/work/z19/z19/lparisi/.spack-0.23.0/opt/spack/linux-sles15-zen2/gcc-11.2.0/libiconv-1.17-suuqhan57tcpbrs4px6rzksm75kr4n5r/include
+LIBARCHIVE_FFLAGS=/work/z19/z19/lparisi/.spack-0.23.0/opt/spack/linux-sles15-zen2/gcc-11.2.0/libiconv-1.17-suuqhan57tcpbrs4px6rzksm75kr4n5r/lib
